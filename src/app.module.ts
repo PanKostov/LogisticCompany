3f65f6e2e6c6599646567a1fdc7c2920cc9bccc4
hzd1719 <panayot.2k@gmail.com>
2023-07-17 22:18:58 +0300

    deleted user repository and app.controller and app.service because they were not used

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user/user.entity';
 import { UserModule } from './user/user.module';
@@ -34,7 +32,5 @@ import { CustomerModule } from './customer/customer.module';
     PacketModule,
     CustomerModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
